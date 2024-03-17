3c1e0758065f19973368d7381555dca318669245
tuanliaaaa <nhattuan44t@gmail.com>
2024-03-17 16:08:31 +0700

    fixed miss library

--- src/main/java/com/g11/savingtrack/repository/EmployeeRepository.java
+++ src/main/java/com/g11/savingtrack/repository/EmployeeRepository.java
@@ -1,9 +1,14 @@
 package com.g11.savingtrack.repository;
 
+import com.g11.savingtrack.dtos.LoginResponse;
 import com.g11.savingtrack.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    Optional<Employee> findByUsername(String username);
 }
