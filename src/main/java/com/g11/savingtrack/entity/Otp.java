06a0e268f248099b8c1d3988dd643c0e9702c14e
Nguyen Thi Hau <82701879+Vitkonlontonn@users.noreply.github.com>
2024-03-24 15:21:28 +0700

    feat[base]: update base

--- src/main/java/com/g11/savingtrack/entity/Otp.java
+++ src/main/java/com/g11/savingtrack/entity/Otp.java
@@ -1,4 +1,16 @@
 package com.g11.savingtrack.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Otp {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  @ManyToOne
+  @JoinColumn(name = "customer_id")
+  private Customer customer;
+  private String code;
 }
