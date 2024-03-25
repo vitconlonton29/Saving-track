1f72c0a6e415c98f212203bfefd9c0881afcd94d
tuanliaaaa <nhattuan44t@gmail.com>
2024-03-25 11:34:16 +0700

    ahihi

--- src/main/java/com/g11/savingtrack/service/impl/WithdrawalServiceImpl.java
+++ src/main/java/com/g11/savingtrack/service/impl/WithdrawalServiceImpl.java
@@ -3,6 +3,7 @@ package com.g11.savingtrack.service.impl;
 import com.g11.savingtrack.dto.request.WithdrawalRequest;
 import com.g11.savingtrack.dto.response.WithdrawalResponse;
 import com.g11.savingtrack.entity.Customer;
+import com.g11.savingtrack.entity.Otp;
 import com.g11.savingtrack.entity.Passbook;
 import com.g11.savingtrack.entity.Receipt;
 import com.g11.savingtrack.exception.customer.CustomerNotFoundException;
@@ -11,11 +12,14 @@ import com.g11.savingtrack.repository.CustomerRepository;
 import com.g11.savingtrack.repository.PassbookRepository;
 import com.g11.savingtrack.repository.ReceiptRepository;
 import com.g11.savingtrack.service.EmailService;
+import com.g11.savingtrack.service.OtpService;
 import com.g11.savingtrack.service.WithdrawalService;
+import com.g11.savingtrack.utils.EmailUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.Random;
 
@@ -26,7 +30,9 @@ public class WithdrawalServiceImpl implements WithdrawalService {
   private final ReceiptRepository receiptRepository;
   private final CustomerRepository customerRepository;
   private final PassbookRepository passbookRepository;
+  private final OtpService otpService;
   private final EmailService emailService;
+
 
   @Override
   public WithdrawalResponse withDraw(WithdrawalRequest request) {
@@ -35,6 +41,20 @@ public class WithdrawalServiceImpl implements WithdrawalService {
     Passbook passbook = passbookRepository.findById(request.getPassbookId()).orElseThrow(PassbookNotFoundException::new);
     Customer customer = customerRepository.findById(request.getCustomerId()).orElseThrow(CustomerNotFoundException::new);
     //gui OTP qua mail de xac thuc
+    EmailUtils emailUtils= new EmailUtils();
+    emailUtils.setSubject("verify rut tien");
+    Random random = new Random();
+    int rad= random.nextInt(900000) + 100000;
+    emailUtils.setMsgBody("ma xac thuc cua bn la "+String.valueOf(rad));
+    emailUtils.setRecipient(customer.getEmail());
+    emailService.sendSimpleMail(emailUtils);
+    Otp otp = new Otp();
+    otp.setCustomer(customer);
+    otp.setCode(String.valueOf(rad));
+    otp.setDateTimeCreate(LocalDateTime.now());
+    otpService.saveOtp(otp);
+
+
 
     long amountWithdraw;
     long amount = passbook.getAmount();
